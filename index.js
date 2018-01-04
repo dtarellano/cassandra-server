9d7b73d739a01538c6627ffd7a189c834291bf14
David Arelano <dtarellano1@gmail.com>
2018-01-03 21:20:29 -0800

    fixed data paths to send to db

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const BodyParser = require('body-parser');
 const cassandra = require('cassandra-driver');
+const db = require('./cassandradb.js').db;
 let queue = [];
 const app = express();
 app.use(BodyParser.json());
-app.post('/dispatch', (request, response) => {
+app.get('/dispatch', (request, response) => {
 	const limit = queue.length;
-	const driver = driverSelector(request.body.drivers);
-	const waitTime = getWaitEstimate(driver, request.body.rider_loc);
 	if (limit >= 100) {
 		db
 			.batch(queue, { prepare: true })
@@ -23,7 +22,12 @@ app.post('/dispatch', (request, response) => {
 		queue = [];
 	}, 5000);
 	queue.push(
-		analytics(request.body.ride_id, driver, request.body.rider_loc, waitTime)
+		analytics(
+			request.body.ride_id,
+			request.body.driver,
+			request.body.rider_loc,
+			request.body.waitTime
+		)
 	);
 	response.status(200).end();
 });
